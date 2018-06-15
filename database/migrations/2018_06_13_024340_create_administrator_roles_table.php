e03b147d6211a1895f89f521fe6c452509314971
Esmeraldo Barios de Guzman Jr <deguzman.esmeraldo@gmail.com>
2018-06-15 11:17:50 +0800

    Add Administrator related models, migrations, seeders and controller

---
+++ database/migrations/2018_06_13_024340_create_administrator_roles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdministratorRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('administrator_roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('administrator_id')->unique();
+            $table->unsignedInteger('role_id')->unique();
+            $table->unsignedInteger('assigned_by');
+            $table->unsignedInteger('revoked_by')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('administrator_roles');
+    }
+}
