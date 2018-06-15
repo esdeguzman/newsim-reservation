2206903350753d2e731f0be2d03310599103bd11
Esmeraldo Barios de Guzman Jr <deguzman.esmeraldo@gmail.com>
2018-06-15 11:23:04 +0800

    Add Role model, controller, migration and seeder

---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,60 @@
+<?php
+
+use App\Role;
+use Illuminate\Database\Seeder;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::create([
+            'name' => 'developer',
+            'added_by' => 1,
+        ]);
+
+        Role::create([
+            'name' => 'system admin',
+            'added_by' => 1,
+        ]);
+
+        Role::create([
+            'name' => 'accounting officer',
+            'added_by' => 1,
+        ]);
+
+        Role::create([
+            'name' => 'confirm reservation',
+            'added_by' => 1,
+        ]);
+
+        Role::create([
+            'name' => 'cancel reservation',
+            'added_by' => 1,
+        ]);
+
+        Role::create([
+            'name' => 'registration officer',
+            'added_by' => 1,
+        ]);
+
+        Role::create([
+            'name' => 'confirm registration',
+            'added_by' => 1,
+        ]);
+
+        Role::create([
+            'name' => 'training officer',
+            'added_by' => 1
+        ]);
+
+        Role::create([
+            'name' => 'marketing officer',
+            'added_by' => 1
+        ]);
+    }
+}
