1dbc365ef0564b2a3023fdafd76b04eb57ca0726
Esmeraldo Barios de Guzman Jr <deguzman.esmeraldo@gmail.com>
2018-06-20 10:59:07 +0800

    Update course show view to load dynamic content

--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -2,56 +2,56 @@
 @section('page-tab-title') Courses @stop
 @section('active-page')
     <li><a href="{{ route('courses.index') }}"><b class="text-info">Courses</b></a></li>
-    <li class="active"><span class="text-muted text-uppercase">bosiet</span></li>
+    <li class="active"><span class="text-muted text-uppercase">{{ $course->code }}</span></li>
 @stop
 @section('page-short-description') @stop
 @section('page-content')
     <div class="col-md-12 block3">
         <div class="white-box printableArea">
-            <button class="pull-right text-uppercase btn btn-info"  data-toggle="modal" data-target=".update-course">update course details</button>
-            <h3 class="text-uppercase">bosiet <span class="tooltip-item2"><small>basic offshore safety induction and emergency training</small></span></h3>
+            <button class="pull-right text-uppercase btn btn-info"  data-toggle="modal" data-target=".update-course" id="btn_edit_course">update course details</button>
+            <h3 class="text-uppercase">{{ $course->code }} <span class="tooltip-item2"><small>{{ $course->description }}</small></span></h3>
             <hr>
             <div class="row">
                 <div class="col-md-12">
-                    <div class="pull-left"> <address>
-                            <h1> &nbsp;<b class="text-uppercase">P 3,000.00</b><sup><small>original price</small></sup></h1>
-                        </address> </div>
-                    <div class="pull-right text-right"> <address>
-                            <p class="m-t-30"><button class="text-uppercase btn btn-danger" data-toggle="modal" data-target=".update-original-price">amend original price</button>
-                            {{--<p><b>Reservation Date :</b> <i class="fa fa-calendar"></i> May 31, 2018</p>--}}
-                            {{--<p><b class="text-danger">Expiration Date :</b> <i class="fa fa-calendar"></i> June 1, 2018</p>--}}
-                        </address> </div>
                 </div>
-                <div class="col-md-12 m-t-40">
+                <div class="col-md-12 m-t-20">
                     <div class="table-responsive" style="clear: both;">
                         <h3 class="text-uppercase"><code>timeline</code></h3>
-                        <p class="text-muted">This shows important events happened to this schedule like <code class="text-uppercase">original price</code> changes and more!</p>
+                        <p class="text-muted">This shows important events happened to this course like <code class="text-uppercase">description</code> changes and more!</p>
                         <table class="table table-hover">
                             <thead>
                             <tr>
-                                <th>Event</th>
-                                <th>Blame</th>
+                                <th>Code</th>
+                                <th>Description</th>
+                                <th>Responsible</th>
                                 <th>Date Amended</th>
                                 <th>Remarks</th>
                             </tr>
                             </thead>
                             <tbody>
+                            @if($courseHistory->count() > 0)
+                                @foreach($courseHistory->sortByDesc('created_at') as $history)
+                                <tr>
+                                    <td class="text-uppercase">{{ $history->code }}</td>
+                                    <td class="text-uppercase">{{ $history->description }}</td>
+                                    <td class="text-uppercase">{{ $history->updatedBy->administrator->full_name }}</td>
+                                    <td class="text-uppercase">{{ Carbon\Carbon::parse($history->created_at)->toFormattedDateString() }}</td>
+                                    <td class="text-uppercase">{{ $history->remarks }}</td>
+                                </tr>
+                                @endforeach
+                            @else
                             <tr>
-                                <td class="text-uppercase">
-                                    updated <code>original price</code> from <code>P 2,500.00</code> to <code>P 3,000.00</code>
-                                </td>
-                                <td class="text-uppercase">marketing officer</td>
-                                <td class="text-uppercase">june 24, 2018</td>
-                                <td class="text-uppercase">as per department advise</td>
+                                <td class="text-center" colspan="5"><b class="text-uppercase">No amendments have been retrieved.</b></td>
                             </tr>
+                            @endif
                             </tbody>
                         </table>
                     </div>
                 </div>
-                <div class="col-lg-5 col-sm-5 pull-right m-t-40">
+                <div class="col-lg-5 col-sm-6 col-xs-12 pull-right m-t-40">
                     <div class="panel panel-danger">
                         <div class="panel-heading"> Danger Zone
-                            <div class="pull-right"><a href="#" data-perform="panel-collapse"><i class="ti-minus"></i></a> </div>
+                            <div class="pull-right"><a href="#" data-perform="panel-collapse"><i class="ti-plus"></i></a> </div>
                         </div>
                         <div class="panel-wrapper collapse" aria-expanded="false" style="height: 0px;">
                             <div class="panel-body">
@@ -59,9 +59,9 @@
                                     This panel is <b class="text-info text-uppercase">purposely closed by default</b>, if you are reading this,
                                     and <b class="text-danger text-uppercase">still continue to delete this course</b>; a log will be saved tagging you
                                     as the sole administrator that is <b class="text-info text-uppercase">responsible for this deletion</b>.
-                                </p> <br/><br/>
-                                <button class="btn btn-block btn-danger text-uppercase"  data-toggle="modal" data-target=".delete-course">i understand, proceed course deletion</button>
+                                </p>
                             </div>
+                            <button class="btn btn-block btn-danger text-uppercase"  data-toggle="modal" data-target=".delete-course">i understand, proceed course deletion</button>
                         </div>
                     </div>
                 </div>
@@ -78,7 +78,8 @@
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
                     <h4 class="modal-title text-uppercase" id="deleteCourseLabel">confirm action</h4> </div>
-                <form action="#">
+                <form action="{{ route('courses.destroy', $course->id) }}" method="post">
+                    {{ csrf_field() }} {{ method_field('delete') }}
                     <div class="modal-body">
                         Deleting a course requires <b class="text-uppercase text-info">remarks</b> for future reference. <br/><br/>
                         <textarea class="form-control form-material" name="remarks" rows="3"></textarea>
@@ -108,17 +109,23 @@
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
                     <h4 class="modal-title text-uppercase" id="updateCourseLabel">confirm action</h4> </div>
-                <form action="#">
+                <form action="{{ route('courses.update', $course->id) }}" method="post">
+                    {{ csrf_field() }} {{ method_field('put') }}
+                    <input type="text" class="hidden" name="redirect_path" value="{{ route('courses.show', $course->id) }}" />
                     <div class="modal-body">
                         <div class="form-group">
-                            <label for="code">Code</label>
-                            <input type="text" class="form-control form-material" name="code" id="code" value="bosiet" />
+                            <label for="code">Code *</label>
+                            <input type="text" class="form-control form-material" name="code" id="code" value="{{ $course->code }}" />
                         </div>
                         <div class="form-group">
-                            <label for="code">Description</label>
-                            <input type="text" class="form-control form-material" name="description" id="description" value="basic offshore safety induction and emergency training" />
+                            <label for="code">Description *</label>
+                            <input type="text" class="form-control form-material" name="description" id="description" value="{{ $course->description }}" />
                         </div>
-                        <p class="text-muted m-t-15">All schedules and reservations associated with this course will also be affected.</p>
+                        <div class="form-group">
+                            <label for="remarks">Remarks *</label>
+                            <textarea type="text" class="form-control form-material" name="remarks" id="remarks" cols="3"></textarea>
+                        </div>
+                        <p class="text-danger text-uppercase m-t-15">All branch courses associated with this course will also be affected.</p>
                     </div>
                     <div class="modal-footer">
                         <button class="btn btn-info text-uppercase" data-dismiss="modal">cancel</button>
@@ -129,29 +136,6 @@
         </div>
     </div>
     <!-- /update course -->
-
-    <!-- update original price -->
-    <div class="modal fade update-original-price" tabindex="-1" role="dialog" aria-labelledby="updateOriginalPriceLabel" aria-hidden="true" style="display: none;">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                    <h4 class="modal-title text-uppercase" id="updateOriginalPriceLabel">confirm action</h4> </div>
-                <form action="#">
-                    <div class="modal-body">
-                        <label for="value">Original Price</label>
-                        <input type="text" class="form-control form-material money-mask" name="value" id="value" value="300000" />
-                        <p class="text-muted m-t-5">Only <b class="text-info">new reservations</b> will be affected by this price update.</p>
-                    </div>
-                    <div class="modal-footer">
-                        <button class="btn btn-info text-uppercase" data-dismiss="modal">cancel</button>
-                        <button class="btn btn-danger text-uppercase submit">update</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-    <!-- /update original price -->
 
     <!-- /modals -->
 @stop
@@ -181,9 +165,9 @@
             setTimeout(removeHighlight, 100)
             // highlight workaround end
 
-            $('#code, #description').on('keyup', function() {
-                $(this).val( $(this).val().toLowerCase() )
+            $('#code, #description').on('keyup', function () {
+                $(this).val($(this).val().toLowerCase())
             })
-        });
+        })
     </script>
 @stop
