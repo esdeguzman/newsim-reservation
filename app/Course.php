c39581714b4518ef930b7ab4c2afc5a4a01a88eb
Esmeraldo Barios de Guzman Jr <deguzman.esmeraldo@gmail.com>
2018-06-26 13:51:01 +0800

    Refactor HasHistory trait to Historiable on course model

--- app/Course.php
+++ app/Course.php
@@ -2,13 +2,13 @@
 
 namespace App;
 
-use App\Traits\HasHistory;
+use App\Traits\Historiable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Course extends Model
 {
-    use SoftDeletes, HasHistory;
+    use SoftDeletes, Historiable;
 
     protected $guarded = [];
 }
