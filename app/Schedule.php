cbf2824a5854ab63be701c6e13161729eca35a60
Esmeraldo Barios de Guzman Jr <deguzman.esmeraldo@gmail.com>
2018-06-26 14:06:38 +0800

    Add branch, branchCourse, monthName and discountPercentage methods
    added methods above on Schedule model

---
+++ app/Schedule.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use App\Traits\Historiable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Schedule extends Model
+{
+    use SoftDeletes, Historiable;
+
+    protected $guarded = [];
+
+    public function branch()
+    {
+        return $this->belongsTo(Branch::class);
+    }
+
+    public function branchCourse()
+    {
+        return $this->belongsTo(BranchCourse::class);
+    }
+
+    public function monthName()
+    {
+        $dateObject = \DateTime::createFromFormat('!m', $this->month);
+        return $dateObject->format('F');
+    }
+
+    public function discountPercentage()
+    {
+        return $this->discount * 100 . '%';
+    }
+}
