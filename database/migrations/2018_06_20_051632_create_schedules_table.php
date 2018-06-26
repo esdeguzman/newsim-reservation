4297d96f0ddf072c9ba57121b9682e1d10909049
Esmeraldo Barios de Guzman Jr <deguzman.esmeraldo@gmail.com>
2018-06-26 14:09:01 +0800

    Add schedules table migration

---
+++ database/migrations/2018_06_20_051632_create_schedules_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('schedule_id')->nullable();
+            $table->unsignedInteger('branch_id');
+            $table->unsignedInteger('branch_course_id');
+            $table->unsignedSmallInteger('month');
+            $table->unsignedSmallInteger('year');
+            $table->unique(['branch_course_id', 'month']);
+            $table->string('status')->default('new');
+            $table->decimal('discount', 3, 2);
+            $table->unsignedInteger('added_by');
+            $table->unsignedInteger('updated_by')->nullable();
+            $table->unsignedInteger('deleted_by')->nullable();
+            $table->text('remarks')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
