86bf1a606a8196413147ce1111abdb4f37c54a88
Esmeraldo Barios de Guzman Jr <deguzman.esmeraldo@gmail.com>
2018-06-26 13:50:22 +0800

    Remove relational method updated by on course model

--- app/Course.php
+++ app/Course.php
@@ -11,9 +11,4 @@ class Course extends Model
     use SoftDeletes, HasHistory;
 
     protected $guarded = [];
-
-    public function updatedBy()
-    {
-        return $this->belongsTo(User::class, 'updated_by');
-    }
 }
