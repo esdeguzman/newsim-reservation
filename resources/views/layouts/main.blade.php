664fa2230fdcee90ed61aff9e04ff4695e394ab3
Esmeraldo Barios de Guzman Jr <deguzman.esmeraldo@gmail.com>
2018-06-29 10:12:02 +0800

    Update reservations sidebar where each reservations are separated

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -169,7 +169,16 @@
                         <li> <a href="{{ route('schedules.index') . '?branch=makati' }}"><i class=" fa-fw">&#10095;</i><span class="hide-menu">Makati</span></a> </li>
                     </ul>
                 </li>
-                <li><a href="{{ route('reservations.index') }}" class="waves-effect @yield('reservations-sidebar-menu')" id="reservations-sidebar"><i class="fa fa-tags"></i> <span class="hide-menu">&nbsp;&nbsp;&nbsp;Reservations</span></a></li>
+                <li> <a href="#" class="waves-effect @yield('reservations-sidebar-menu')" id="reservations-sidebar"><i class="fa fa-tags" data-icon="v"></i> <span class="hide-menu">&nbsp;&nbsp;&nbsp;Reservations <span class="fa arrow"></span> <!-- <span class="label label-rouded label-inverse pull-right">4</span> --> </span></a>
+                    <ul class="nav nav-second-level">
+                        <li> <a href="{{ route('reservations.index') }}" id="all-reservations"><i class=" fa-fw">&#10095;</i><span class="hide-menu">All</span></a> </li>
+                        <li> <a href="{{ route('reservations.index') . '?branch=bacolod' }}"><i class=" fa-fw">&#10095;</i><span class="hide-menu">Bacolod</span></a> </li>
+                        <li> <a href="{{ route('reservations.index') . '?branch=cebu' }}"><i class=" fa-fw">&#10095;</i><span class="hide-menu">Cebu</span></a> </li>
+                        <li> <a href="{{ route('reservations.index') . '?branch=davao' }}"><i class=" fa-fw">&#10095;</i><span class="hide-menu">Davao</span></a> </li>
+                        <li> <a href="{{ route('reservations.index') . '?branch=ilo-ilo' }}"><i class=" fa-fw">&#10095;</i><span class="hide-menu">Ilo-ilo</span></a> </li>
+                        <li> <a href="{{ route('reservations.index') . '?branch=makati' }}"><i class=" fa-fw">&#10095;</i><span class="hide-menu">Makati</span></a> </li>
+                    </ul>
+                </li>
                 <li class="divider"></li>
                 <li> <a href="#" class="waves-effect @yield('user-management-sidebar-menu')" id="user-management-sidebar"><i class="fa fa-users" data-icon="v"></i> <span class="hide-menu">&nbsp;&nbsp;&nbsp;User Management <span class="fa arrow"></span> <!-- <span class="label label-rouded label-inverse pull-right">4</span> --> </span></a>
                     <ul class="nav nav-second-level">
