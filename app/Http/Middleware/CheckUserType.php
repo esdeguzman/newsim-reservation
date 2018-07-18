9e875c8aaca86961b2981d2d294ba3d9abf98ea3
Esmeraldo Barios de Guzman Jr <deguzman.esmeraldo@gmail.com>
2018-07-18 13:30:57 +0800

    Make notifications for new, paid and registered reservations
    for both admin and trainee pages

--- app/Http/Middleware/CheckUserType.php
+++ app/Http/Middleware/CheckUserType.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Middleware;
 
+use function App\Helper\admin;
+use function App\Helper\trainee;
+use App\PaymentTransaction;
+use App\Reservation;
 use Closure;
 
 class CheckUserType
@@ -17,9 +21,38 @@ class CheckUserType
     {
         $prefix = str_replace('/', '', $request->route()->getPrefix());
         if (optional(auth()->user()->administrator)->exists() && $prefix == 'admin') {
-        // do nothing
+            $newReservationsCount = Reservation::where('status', 'new')
+                                                    ->where('branch_id', admin()->branch_id)
+                                                    ->where('seen', 0)->get()->count();
+
+            $newPaymentsCount = PaymentTransaction::whereHas('reservation', function ($query) {
+                                                        $query->where('branch_id', admin()->branch_id);
+                                                    })->where('status', 'new')
+                                                    ->where('seen', 0)->get()->count();
+
+            $newPaidReservationsCount = Reservation::where('status', 'paid')->where('seen', 0)->get()->count();
+
+            view()->share([
+                'newReservations' => $newReservationsCount,
+                'newPayments' => $newPaymentsCount,
+                'newPaidReservations' => $newPaidReservationsCount,
+            ]);
         } elseif (optional(auth()->user()->trainee)->exists() && $prefix == 'trainee') {
-            // do nothing
+            $newPaymentConfirmationsCount = PaymentTransaction::whereHas('reservation', function ($query) {
+                                                $query->where('trainee_id', trainee()->id);
+                                            })->where('status', 'confirmed')
+                                            ->where('seen', 0)
+                                            ->get()->count();
+
+            $newRegisteredCoursesCount = Reservation::where('trainee_id', trainee()->id)
+                                                        ->where('status', 'registered')
+                                                        ->where('seen', 0)
+                                                        ->get()->count();
+
+            view()->share([
+                'newPaymentConfirmations' => $newPaymentConfirmationsCount,
+                'newRegisteredCourses' => $newRegisteredCoursesCount,
+            ]);
         } else {
             return redirect('page-not-found');
         }
