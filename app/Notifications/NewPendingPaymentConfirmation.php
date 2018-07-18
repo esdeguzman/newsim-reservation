d1b3a163c966ec4ce81c4a6fae8133847b61326e
Esmeraldo Barios de Guzman Jr <deguzman.esmeraldo@gmail.com>
2018-07-18 13:32:49 +0800

    Create reservation email notifications for registered, confirmed payment

---
+++ app/Notifications/NewPendingPaymentConfirmation.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Reservation;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class NewPendingPaymentConfirmation extends Notification
+{
+    use Queueable;
+
+    protected $reservation;
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(Reservation $reservation)
+    {
+        $this->reservation = $reservation;
+    }
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->greeting('Payment transaction number has been received!')
+            ->line('Your payment transaction number for reservation code: '. $this->reservation->code . ' has been received and is now awaiting for confirmation!')
+            ->line('Our accounting officer will update your reservation information if your payment is already been reflected to our account. You will also received a notification upon payment confirmation!');
+    }
+
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
