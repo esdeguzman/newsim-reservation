16e260d6a1132fad5dc3de8b44598cf051b2e96b
Esmeraldo Barios de Guzman Jr <deguzman.esmeraldo@gmail.com>
2018-07-18 18:58:34 +0800

    Add/refactor reservations related routes

--- routes/web.php
+++ routes/web.php
@@ -21,16 +21,18 @@ Route::get('page-not-found', function () { return view('pages.404'); });
 
 Route::prefix('admin')->middleware('auth', 'can.access')->group(function () {
     Route::get('home', function () { return view('layouts.main'); })->name('admin.home');
+    Route::get('reservations', 'ReservationsController@index')->name('reservations.index');
+    Route::get('reservations/{reservation}', 'ReservationsController@show')->name('reservations.show');
+    Route::get('reservations/{reservation}/all', 'ReservationsController@index')->name('reservations.show-all');
 });
 
 Route::prefix('trainee')->middleware('auth', 'can.access')->group(function () {
     Route::get('home', 'TraineesController@home')->name('trainee.home');
+    Route::get('schedules', 'TraineesController@schedules')->name('trainee-schedules');
+    Route::get('schedules/{schedule}', 'TraineesController@showSchedule')->name('trainee-schedules.show');
+    Route::get('reservations', 'TraineesController@reservations')->name('trainee-reservations');
+    Route::get('reservations/{reservation}', 'TraineesController@showReservation')->name('trainee-reservations.show');
 });
-
-Route::get('trainee/schedules', 'TraineesController@schedules')->name('trainee.schedules');
-Route::get('trainee/schedules/{schedule}', 'TraineesController@showSchedule')->name('trainee.schedule-show');
-Route::get('trainee/reservations', 'TraineesController@reservations')->name('trainee.reservations');
-Route::get('trainee/reservations/{reservation}', 'TraineesController@showReservation')->name('trainee.reservation-show');
 
 /*
 |--------------------------------------------------------------------------
@@ -38,13 +40,19 @@ Route::get('trainee/reservations/{reservation}', 'TraineesController@showReserva
 |--------------------------------------------------------------------------
 */
 Route::post('authenticate', 'Auth\LoginController@login')->name('login.authenticate');
+Route::post('trainee/signup', 'TraineesController@signUp')->name('trainee.signup');
+Route::post('request-reset-password', 'UsersController@requestPasswordReset')->name('users.request-reset-password');
+Route::get('reset-password/{user}', 'UsersController@resetPassword')->name('users.reset-password');
 
 Route::prefix('admin')->middleware('auth', 'can.access')->group(function () {
     Route::post('logout', 'Auth\LoginController@logout')->name('admin.logout');
+    Route::post('payment-transactions', 'PaymentTransactionsController@store');
 });
 
 Route::prefix('trainee')->middleware('auth', 'can.access')->group(function () {
     Route::post('logout', 'Auth\LoginController@logout')->name('trainee.logout');
+    Route::post('reservations/{schedule}', 'ReservationsController@store')->name('trainee-reservations.store');
+    Route::post('payment-transactions', 'PaymentTransactionsController@store');
 });
 
 /*
@@ -52,6 +60,18 @@ Route::prefix('trainee')->middleware('auth', 'can.access')->group(function () {
 | System Individual UPDATE Routes
 |--------------------------------------------------------------------------
 */
+
+Route::prefix('trainee')->middleware('auth', 'can.access')->group(function () {
+    Route::put('reservations/{reservation}', 'ReservationsController@update');
+});
+
+Route::prefix('admin')->middleware('auth', 'can.access')->group(function () {
+    Route::put('reservations/{reservation}', 'ReservationsController@update');
+    Route::put('reservations/{reservation}/confirm', 'ReservationsController@confirm')->name('reservations.confirm');
+    Route::put('reservations/{reservation}/refund', 'ReservationsController@refund')->name('reservations.refund');
+    Route::put('reservations/{reservation}/registered', 'ReservationsController@registered')->name('reservations.registered');
+    Route::put('courses/{course}/restore', 'CoursesController@restore')->name('courses.restore');
+});
 
 /*
 |--------------------------------------------------------------------------
@@ -65,10 +85,9 @@ Route::prefix('trainee')->middleware('auth', 'can.access')->group(function () {
 |--------------------------------------------------------------------------
 */
 Route::resource('administrators', 'AdministratorsController');
-Route::resource('reservations', 'ReservationsController');
 Route::resource('trainees', 'TraineesController');
 
-Route::prefix('admin')->group(function () {
+Route::prefix('admin')->middleware('auth', 'can.access')->group(function () {
     Route::resource('branch-courses', 'BranchCoursesController');
     Route::resource('courses', 'CoursesController');
     Route::resource('original-prices', 'OriginalPricesController');
