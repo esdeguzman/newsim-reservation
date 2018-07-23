0b454e9ec4a0a8c8edd7af572a58faec22ca4781
Esmeraldo Barios de Guzman Jr <deguzman.esmeraldo@gmail.com>
2018-07-23 09:39:30 +0800

    Add relational method administrators to department model

--- app/Department.php
+++ app/Department.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Department extends Model
 {
     //
+
+    public function administrators()
+    {
+        return $this->hasMany(Administrator::class);
+    }
 }
