ffdb58791859a7215b2b3b8ac81706397a3e0838
Esmeraldo Barios de Guzman Jr <deguzman.esmeraldo@gmail.com>
2018-07-23 15:52:21 +0800

    Add page-specific-css blade directive

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -24,6 +24,7 @@
     <!-- color CSS -->
     <link href="{{ asset('css/colors/megna-dark.css') }}" id="theme" rel="stylesheet">
     @yield('page-specific-link')
+    @yield('page-specific-css')
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
