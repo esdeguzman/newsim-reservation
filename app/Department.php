3d895e6e10e4fac8d413930c1b01031e14a99e44
Esmeraldo Barios de Guzman Jr <deguzman.esmeraldo@gmail.com>
2018-07-23 09:40:50 +0800

    Use soft deletes and historiable traits to department model
    Also, added protected variable guarded to empty array.

--- app/Department.php
+++ app/Department.php
@@ -2,11 +2,15 @@
 
 namespace App;
 
+use App\Traits\Historiable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Department extends Model
 {
-    //
+    use SoftDeletes, Historiable;
+
+    protected $guarded = [];
 
     public function administrators()
     {
