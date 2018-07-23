2b659dcd507696d3870fdd0299c121e89232b6db
Esmeraldo Barios de Guzman Jr <deguzman.esmeraldo@gmail.com>
2018-07-23 09:59:45 +0800

    Add store method for users that requests administrator accounts

--- app/Http/Controllers/AdministratorsController.php
+++ app/Http/Controllers/AdministratorsController.php
@@ -28,4 +28,44 @@ class AdministratorsController extends Controller
     {
         return view('administrators.show');
     }
+
+    public function store(Request $request)
+    {
+        $userDetails = $request->validate([
+            'username' => 'required|min:3|unique:users',
+            'email' => 'required|unique:users',
+            'password' => 'required|min:6|confirmed',
+        ]);
+
+        $adminInfo = $request->validate([
+            'branch_id' => 'required',
+            'department_id' => 'required',
+            'position_id' => 'required',
+            'employee_id' => 'required',
+            'full_name' => 'required',
+        ]);
+
+        $request->validate([
+            'reason' => 'required|min:15'
+        ]);
+
+        $user = User::create($userDetails);
+
+        $adminInfo['user_id'] = $user->id; // add user_id to adminInfo to facilitate the use of create method
+
+        $admin = Administrator::create($adminInfo);
+
+        HistoryDetail::create([
+            'log' => $request->reason,
+            'remarks' => "$request->full_name is requesting an admin account, please see log",
+            'updated_by' => 1,
+        ]);
+
+        $request->session()->flash('info', [
+            'success' => 'Request successfully submitted! You will receive an email that will verification email ' .
+                            'after the administrator has confirmed your request.'
+        ] );
+
+        return back();
+    }
 }
