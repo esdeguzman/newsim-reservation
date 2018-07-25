34f048fbdfcf21ff402c2f24123b55079baeb475
Esmeraldo Barios de Guzman Jr <deguzman.esmeraldo@gmail.com>
2018-07-25 09:44:08 +0800

    Add functions update and updateStatus to administrators controller

--- app/Http/Controllers/AdministratorsController.php
+++ app/Http/Controllers/AdministratorsController.php
@@ -80,4 +80,148 @@ class AdministratorsController extends Controller
 
         return back();
     }
+
+    public function update(Administrator $administrator, Request $request)
+    {
+        $request->validate([
+            'email' => [
+                'required',
+                Rule::unique('users')->ignore($administrator->user->id)
+            ],
+            'employee_id' => [
+                'required',
+                Rule::unique('administrators')->ignore($administrator->id)
+            ],
+            'full_name' => 'required|min:6',
+            'branch_id' => 'required',
+            'department_id' => 'required',
+            'position_id' => 'required',
+        ]);
+
+        try {
+            $user = $administrator->user;
+            $user->email = $request->email;
+            $user->save();
+        } catch (\Exception $exception) {
+            Log::error('admin update error', $exception->errorInfo);
+        }
+
+        try {
+            $administrator->employee_id = $request->employee_id;
+            $administrator->save();
+        } catch (\Exception $exception) {
+            Log::error('admin update error', $exception->errorInfo);
+        }
+
+        $administrator->full_name = $request->full_name;
+        $administrator->branch_id = $request->branch_id;
+        $administrator->department_id = $request->department_id;
+        $administrator->position_id = $request->position_id;
+        $administrator->save();
+
+        $oldAdministratorRoles = AdministratorRole::where('administrator_id', $administrator->id)->get();
+
+        if ($request->has('roles')) {
+            try {
+                $newAdministratorRoles = Role::find($request->roles);
+
+                if ($oldAdministratorRoles) {
+                    foreach ($oldAdministratorRoles as $oldAdministratorRole) {
+                        if (count($newAdministratorRoles) == 0 || ! $newAdministratorRoles
+                                                                    ->contains('id', $oldAdministratorRole->role_id)) {
+                            $oldAdministratorRole->revoked_by = admin()->id;
+                            $oldAdministratorRole->save();
+
+                            HistoryDetail::create([
+                                'administrator_role_id' => $oldAdministratorRole->id,
+                                'log' => 'revoked (' . $oldAdministratorRole->role->name . ') role',
+                                'remarks' => 'role has been revoked',
+                                'updated_by' => admin()->id,
+                            ]);
+                        }
+                    }
+                }
+
+                if (count($newAdministratorRoles) > 0) {
+                    foreach ($newAdministratorRoles as $newAdministratorRole) {
+                        $adminRole = AdministratorRole::where('administrator_id', $administrator->id)
+                            ->where('role_id', $newAdministratorRole->id)->first();
+
+                        if ($adminRole) {
+                          $adminRole->revoked_by = null;
+                          $adminRole->save();
+                        } else {
+                            $adminRole = AdministratorRole::create([
+                                'administrator_id' => $administrator->id,
+                                'role_id' => $newAdministratorRole->id,
+                                'assigned_by' => admin()->id,
+                            ]);
+                        }
+
+                        HistoryDetail::create([
+                            'administrator_role_id' => $adminRole->id,
+                            'log' => 'assigned (' . $adminRole->role->name . ') role',
+                            'remarks' => 'role has been assigned',
+                            'updated_by' => admin()->id,
+                        ]);
+                    }
+                }
+            } catch (\Exception $exception) {
+                return $exception;
+            }
+        } else {
+            if ($oldAdministratorRoles) {
+                foreach ($oldAdministratorRoles as $oldAdministratorRole) {
+                    $oldAdministratorRole->revoked_by = admin()->id;
+                    $oldAdministratorRole->save();
+
+                    HistoryDetail::create([
+                        'administrator_role_id' => $oldAdministratorRole->id,
+                        'log' => 'revoked (' . $oldAdministratorRole->role->name . ') role',
+                        'remarks' => 'role has been revoked',
+                        'updated_by' => admin()->id,
+                    ]);
+                }
+            }
+        }
+
+        $request->session()->flash('info', [
+            'title' => 'Admin Info Updated!',
+            'type' => 'success',
+            'text' => 'Admin information has been successfully updated!',
+            'confirmButtonColor' => '#8db600',
+            'confirmButtonText' => 'OKAY, COOL!',
+        ]);
+
+        return back();
+    }
+
+    public function updateStatus(Administrator $administrator, Request $request)
+    {
+        $request->validate([
+            'status' => 'required',
+            'remarks' => 'required|min:10',
+        ]);
+
+        $oldStatus = $administrator->status;
+        $administrator->status = $request->status;
+        $administrator->save();
+
+        HistoryDetail::create([
+            'administrator_id' => $administrator->id,
+            'log' => "updated admin status from {$oldStatus} to {$administrator->status}, please see remarks",
+            'remarks' => $request->remarks,
+            'updated_by' => admin()->id
+        ]);
+
+        $request->session()->flash('info', [
+            'title' => 'Admin Status Updated!',
+            'type' => 'success',
+            'text' => 'Admin status has been successfully updated!',
+            'confirmButtonColor' => '#8db600',
+            'confirmButtonText' => 'OKAY, COOL!',
+        ]);
+
+        return back();
+    }
 }
