9c0a7a22b2e0779a6453e7870613f641b846d9f6
Esmeraldo Barios de Guzman Jr <deguzman.esmeraldo@gmail.com>
2018-07-25 15:53:07 +0800

    Add isOfficer function to user model

--- app/User.php
+++ app/User.php
@@ -48,6 +48,11 @@ class User extends Authenticatable
         return $this->administrator->roles->where('role_id', 2)->count() > 0 ? true: false;
     }
 
+    public function isOfficer()
+    {
+        return str_contains($this->administrator->position->name, 'officer');
+    }
+
     public function isAdmin()
     {
         return optional($this->administrator)->exists()? true : false;
