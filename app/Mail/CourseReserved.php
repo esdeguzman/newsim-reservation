0bd9a96fe0302fb4531bb9524975fa9163d4bc4a
Esmeraldo Barios de Guzman Jr <deguzman.esmeraldo@gmail.com>
2018-07-26 13:47:09 +0800

    Temporary removed the should queue implementation

--- app/Mail/CourseReserved.php
+++ app/Mail/CourseReserved.php
@@ -8,7 +8,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class CourseReserved extends Mailable implements ShouldQueue
+class CourseReserved extends Mailable
 {
     use Queueable, SerializesModels;
 
